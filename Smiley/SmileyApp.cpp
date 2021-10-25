d6e3956d25a8f65ce5ca0f6ca6a67acf64cbd01c
Sadab Hafiz <sadabhafizny.com>
2021-10-25 17:54:38 -0400

    intro to glfw

--- Smiley/SmileyApp.cpp
+++ Smiley/SmileyApp.cpp
@@ -1,11 +1,16 @@
 #include "pch.h"
 #include "SmileyApp.h"
-
+#include "GLFW/glfw3.h"
 namespace Smiley {
 	void SmileyApp::Run() {
 		std::cout << "Smiley is walking..." << std::endl;
+		GLFWwindow* window;
+		glfwInit();
+		window = glfwCreateWindow(800, 600, "Test",NULL,NULL);
 		while (true) {
 			OnUpdate();
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
 	}
 	void SmileyApp::OnUpdate() {
